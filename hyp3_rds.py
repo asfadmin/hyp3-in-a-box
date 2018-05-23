f216b9c87bb60820e50ea40815a360212dee2f2e
William Horn <wbhorn@alaska.edu>
2018-05-22 16:40:44 -0800

    Troposphere+awscli for making RDS using cloudformation
    Still debuggin, can't seem to connect to db after stack is created

---
+++ hyp3_rds.py
@@ -0,0 +1,57 @@
+# Converted from RDS_with_DBParameterGroup.template located at:
+# http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
+
+from troposphere import Parameter, Ref, Template
+from troposphere.rds import DBInstance
+import sys
+import os
+
+
+t = Template()
+
+t.add_description(
+    "AWS CloudFormation Sample Template RDS_with_DBParameterGroup: Sample "
+    "template showing how to create an Amazon RDS Database Instance with "
+    "a DBParameterGroup.**WARNING** This template creates an Amazon "
+    "Relational Database Service database instance. You will be billed for "
+    "the AWS resources used if you create a stack from this template.")
+
+dbuser = t.add_parameter(Parameter(
+    "DBUser",
+    NoEcho=False,
+    Description="The database admin account username",
+    Type="String",
+    MinLength="1",
+    MaxLength="16",
+    AllowedPattern="[a-zA-Z][a-zA-Z0-9]*",
+    ConstraintDescription=("must begin with a letter and contain only"
+                           " alphanumeric characters.")
+))
+
+dbpassword = t.add_parameter(Parameter(
+    "DBPassword",
+    NoEcho=True,
+    Description="The database admin account password",
+    Type="String",
+    MinLength="8",
+    MaxLength="41",
+    AllowedPattern="[a-zA-Z0-9]*",
+    ConstraintDescription=("must contain only alphanumeric characters "
+                           "and be from 8-41 characters in length.")
+))
+
+# Only certain versions of postgres are supported on the smaller instance types
+# https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
+mydb = t.add_resource(DBInstance(
+    "MyDB",
+    AllocatedStorage="5",
+    DBInstanceClass="db.t2.micro",
+    DBName="hyp3db",
+    Engine="postgres",
+    EngineVersion="9.5.10",
+    MasterUsername=Ref(dbuser),
+    MasterUserPassword=Ref(dbpassword),
+))
+
+with open(sys.argv[1], 'w') as f:
+    f.write(t.to_json())
---
+++ make_rds_stack.sh
@@ -0,0 +1,10 @@
+RDS_TEMPLATE=template/rds.json
+
+python3 hyp3_rds.py $RDS_TEMPLATE
+
+python3 -m awscli cloudformation create-stack  \
+    --stack-name hyp3dbtest  \
+    --template-body file://$(pwd)/$RDS_TEMPLATE \
+    --parameters  \
+        ParameterKey=DBUser,ParameterValue=hyp3user \
+        ParameterKey=DBPassword,ParameterValue=5wzkQw8g9K
