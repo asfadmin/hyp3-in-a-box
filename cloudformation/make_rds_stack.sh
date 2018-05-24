0a5f0d30f9cec860fdc75618456f9ac9d2ff4ef9
William Horn <wbhorn@alaska.edu>
2018-05-24 10:23:38 -0800

    Fix bugs in stack creation script

--- cloudformation/make_rds_stack.sh
+++ cloudformation/make_rds_stack.sh
@@ -1,9 +1,9 @@
 RDS_TEMPLATE=templates/rds.json
 
-UUID=$(uuid)
+PASS=$(pwgen 13 1)
 
 read -p 'Admin Username: ' USERNAME
-echo    'Admin Password: '$UUID
+echo    'Admin Password: '$PASS
 
 python3 tropo/hyp3_rds.py $RDS_TEMPLATE
 
@@ -11,5 +11,5 @@ python3 -m awscli cloudformation create-stack  \
     --stack-name hyp3dbtest  \
     --template-body file://$(pwd)/$RDS_TEMPLATE \
     --parameters  \
-        ParameterKey=DBUser,ParameterValue=USERNAME \
-        ParameterKey=DBPassword,ParameterValue=$UUID
+        ParameterKey=DBUser,ParameterValue=$USERNAME \
+        ParameterKey=DBPassword,ParameterValue=$PASS
