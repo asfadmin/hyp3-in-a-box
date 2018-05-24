edbf13c58246d05baa6f2628eca57f246961b05f
William Horn <wbhorn@alaska.edu>
2018-05-24 14:57:32 -0800

    Add security group to rds db
    Still need to pass in VpcId as parameter. Need to look into creating one
    in the template to use in the future.

--- cloudformation/tropo/hyp3_rds.py
+++ cloudformation/tropo/hyp3_rds.py
@@ -1,12 +1,15 @@
 # Converted from RDS_with_DBParameterGroup.template located at:
 # http://aws.amazon.com/cloudformation/aws-cloudformation-templates/
 
-from troposphere import Parameter, Ref, Template
-from troposphere.rds import DBInstance
+# Parameter, Ref, Template
+import troposphere as ts
+# import DBInstance
+import troposphere.rds as rds
+import troposphere.ec2 as ec2
 import sys
 
 
-t = Template()
+t = ts.Template()
 
 t.add_description(
     "AWS CloudFormation Sample Template RDS_with_DBParameterGroup: Sample "
@@ -15,7 +18,8 @@ t.add_description(
     "Relational Database Service database instance. You will be billed for "
     "the AWS resources used if you create a stack from this template.")
 
-dbuser = t.add_parameter(Parameter(
+
+dbuser = t.add_parameter(ts.Parameter(
     "DBUser",
     NoEcho=False,
     Description="The database admin account username",
@@ -27,7 +31,7 @@ dbuser = t.add_parameter(Parameter(
                            " alphanumeric characters.")
 ))
 
-dbpassword = t.add_parameter(Parameter(
+dbpassword = t.add_parameter(ts.Parameter(
     "DBPassword",
     NoEcho=True,
     Description="The database admin account password",
@@ -39,17 +43,39 @@ dbpassword = t.add_parameter(Parameter(
                            "and be from 8-41 characters in length.")
 ))
 
+vpcid_param = t.add_parameter(ts.Parameter(
+    "MyVpcId",
+    Description="VpcId of your existing Virtual Private Cloud (VPC)",
+    Type="String",
+))
+
+rule = ec2.SecurityGroupRule(
+    IpProtocol="tcp",
+    FromPort="5432",
+    ToPort="5432",
+    CidrIp="0.0.0.0/0"
+)
+
+t.add_resource(ec2.SecurityGroup(
+    "TCPAll",
+    GroupDescription="Allow for all tcp traffic through port 5432",
+    VpcId=ts.Ref(vpcid_param),
+    SecurityGroupEgress=[rule],
+    SecurityGroupIngress=[rule]
+))
+
+
 # Only certain versions of postgres are supported on the smaller instance types
 # https://docs.aws.amazon.com/AmazonRDS/latest/UserGuide/Concepts.DBInstanceClass.html
-mydb = t.add_resource(DBInstance(
+mydb = t.add_resource(rds.DBInstance(
     "MyDB",
     AllocatedStorage="5",
     DBInstanceClass="db.t2.micro",
     DBName="hyp3db",
     Engine="postgres",
     EngineVersion="9.5.10",
-    MasterUsername=Ref(dbuser),
-    MasterUserPassword=Ref(dbpassword),
+    MasterUsername=ts.Ref(dbuser),
+    MasterUserPassword=ts.Ref(dbpassword),
 ))
 
 with open(sys.argv[1], 'w') as f:
