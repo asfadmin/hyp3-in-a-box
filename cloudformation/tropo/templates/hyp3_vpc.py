41bca26379768024daaf56522b646850311e299a
William Horn <wbhorn@alaska.edu>
2018-05-30 10:25:20 -0800

    Use cloudformation functions to get Availability Zones in template

--- cloudformation/tropo/templates/hyp3_vpc.py
+++ cloudformation/tropo/templates/hyp3_vpc.py
@@ -42,9 +42,17 @@ default_public_route = t.add_resource(ec2.Route(
     DependsOn='Hyp3GatewayAttachment'
 ))
 
+
+def get_az(index):
+    return ts.Select(
+        str(index),
+        ts.GetAZs(ts.Ref('AWS::Region'))
+    )
+
+
 public_net_1 = t.add_resource(ec2.Subnet(
     'Hyp3PublicSubnet1',
-    AvailabilityZone='us-west-2b',
+    AvailabilityZone=get_az(0),
     CidrBlock='10.0.1.0/24',
     MapPublicIpOnLaunch=True,
     VpcId=ts.Ref(hyp3_vpc)
@@ -58,7 +66,7 @@ public_route_association_1 = t.add_resource(ec2.SubnetRouteTableAssociation(
 
 public_net_2 = t.add_resource(ec2.Subnet(
     'Hyp3PublicSubnet2',
-    AvailabilityZone='us-west-2a',
+    AvailabilityZone=get_az(1),
     CidrBlock='10.0.2.0/24',
     MapPublicIpOnLaunch=True,
     VpcId=ts.Ref(hyp3_vpc)
