e308f8208410e3a8f593c71109340cb49ba243e2
William Horn <wbhorn@alaska.edu>
2018-06-05 16:04:13 -0800

    Move polices into json files and load them dynamically

--- cloudformation/tropo/templates/hyp3_find_new.py
+++ cloudformation/tropo/templates/hyp3_find_new.py
@@ -4,47 +4,38 @@ import troposphere as ts
 from troposphere import awslambda
 from troposphere import iam
 
+from . import utils
+
 print('adding find_new lambda')
 
 
 AppendItemToListFunction = t.add_resource(awslambda.Function(
-    "AppendItemToListFunction",
+    "Hyp3FindNewGranulesFunction",
     Code=awslambda.Code(
-        S3Bucket="some-bucket",
-        S3Key="my-code.zip"
+        S3Bucket="hyp3-in-a-box-lambdas",
+        S3Key="find_new_granules.zip"
     ),
-    Handler="index.handler",
+    Handler="lambda_function.lambda_handler",
     Role=ts.GetAtt("LambdaExecutionRole", "Arn"),
     Runtime="python3.6",
     MemorySize=128,
     Timeout=60
 ))
 
-lambda_policy = iam.Policy(
-    PolicyName="root",
-    PolicyDocument={
-        "Version": "2012-10-17",
-        "Statement": [{
-            "Action": ["logs:*"],
-            "Resource": "arn:aws:logs:*:*:*",
-            "Effect": "Allow"
-        }]
-    })
+logs_policy = iam.Policy(
+    PolicyName="LogAccess",
+    PolicyDocument=utils.get_policy('logs-policy')
+)
 
-policy_doc = {
-    "Version": "2012-10-17",
-    "Statement": [{
-        "Action": ["sts:AssumeRole"],
-        "Effect": "Allow",
-        "Principal": {
-            "Service": ["lambda.amazonaws.com"]
-        }
-    }]
-}
+prev_time_s3_policy = iam.Policy(
+    PolicyName='PreviousTimeS3ReadWriteAccess',
+    PolicyDocument=utils.get_policy('previous-time-read-write')
+)
+
 
 LambdaExecutionRole = t.add_resource(iam.Role(
     "LambdaExecutionRole",
     Path="/",
-    Policies=[lambda_policy],
-    AssumeRolePolicyDocument=policy_doc,
+    Policies=[logs_policy, prev_time_s3_policy],
+    AssumeRolePolicyDocument=utils.get_policy('lambda-policy-doc'),
 ))
