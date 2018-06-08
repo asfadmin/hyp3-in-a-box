9751bda857f63c116276b2fba6a99b94df5c5283
Rohan Weeden <askaholic907@gmail.com>
2018-06-08 15:17:38 -0800

    Revert "Trying without the acl again"
    This reverts commit 344ffeff6134818dcec76ea68a58a43cde0422d4.

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 docs() {
     cd docs && make clean html && cd ..
-    aws s3 cp docs/_build/html s3://asf-docs/hyp3-in-a-box --recursive
+    aws s3 cp docs/_build/html s3://asf-docs/hyp3-in-a-box --recursive --acl bucket-owner-full-control
 }
 
 if [ "$1" = "docs" ]
