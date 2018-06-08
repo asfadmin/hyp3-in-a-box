344ffeff6134818dcec76ea68a58a43cde0422d4
Rohan Weeden <askaholic907@gmail.com>
2018-06-08 15:15:23 -0800

    Trying without the acl again

--- upload.sh
+++ upload.sh
@@ -2,7 +2,7 @@
 
 docs() {
     cd docs && make clean html && cd ..
-    aws s3 cp docs/_build/html s3://asf-docs/hyp3-in-a-box --recursive --acl bucket-owner-full-control
+    aws s3 cp docs/_build/html s3://asf-docs/hyp3-in-a-box --recursive
 }
 
 if [ "$1" = "docs" ]
