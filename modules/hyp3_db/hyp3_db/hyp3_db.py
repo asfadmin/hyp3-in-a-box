4746dfb3773265cbd49c5236443cb9baa58deb64
William Horn <wbhorn@alaska.edu>
2018-06-11 11:56:14 -0800

    Set status hyp3db function

--- modules/hyp3_db/hyp3_db/hyp3_db.py
+++ modules/hyp3_db/hyp3_db/hyp3_db.py
@@ -4,6 +4,19 @@ from .hyp3_models import Subscription, LocalQueue
 
 
 class Hyp3DB:
+    valid_job_status = [
+        'INVALID',
+        'INSERTED',
+        'ERROR',
+        'HOLD',
+        'FAILED',
+        'SUCCEEDED',
+        'PROCESSING',
+        'COMPLETE',
+        'QUEUED',
+        'CANCELLED'
+    ]
+
     def __init__(self):
         pass
 
@@ -17,13 +30,26 @@ class Hyp3DB:
 
     def get_job(self, job_id):
         return session \
-            .query(LocalQueue).limit(5).all()
+            .query(LocalQueue) \
+            .filter(LocalQueue.id == job_id) \
+            .one()
 
     def add_job(self, job):
         pass
 
-    def update_job(self, job):
-        pass
+    def set_job_status(self, job, status):
+        if status not in Hyp3DB.valid_job_status:
+            raise ValueError(f'{status} not a valid job_status.')
+
+        if isinstance(job, LocalQueue):
+            job.status = status
+        else:
+            job_id = job
+            session.query(LocalQueue) \
+                .filter(LocalQueue.id == job_id) \
+                .update({'status': status})
+
+        session.commit()
 
     def add_products(self):
         pass
