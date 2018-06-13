bb5850b7852ba42d47920dd3e085148977956316
William Horn <wbhorn@alaska.edu>
2018-06-13 13:08:50 -0800

    Add user to email-package.json

--- modules/hyp3_events/src/hyp3_events/__init__.py
+++ modules/hyp3_events/src/hyp3_events/__init__.py
@@ -1,5 +1,7 @@
+from .hyp3_event import Hyp3Event
 from .notify_only_event import NotifyOnlyEvent
 from .start_event import StartEvent
 from .finish_event import FinishEvent
 
-__all__ = ['NotifyOnlyEvent', 'StartEvent', 'FinishEvent']
+__all__ = ['NotifyOnlyEvent', 'StartEvent', 'FinishEvent', 'Hyp3Event']
+
