7546c4e5c5863eea93b37949587713dafef25e59
Rohan Weeden <askaholic907@gmail.com>
2018-06-14 15:38:31 -0800

    Added docstrings for setup_db lambda

--- lambdas/setup_db/src/lambda_function.py
+++ lambdas/setup_db/src/lambda_function.py
@@ -17,12 +17,23 @@ GRANT ALL PRIVILEGES ON DATABASE hyp3db to hyp3_user;
 """)
 
 
-def lambda_handler(event, context):
+def lambda_handler(aws_event, aws_context):
+    """ AWS Lambda entry point. Renders an email for the given parameters and
+    sends it via SES.
+
+        :param aws_event: lambda event data
+        :param aws_context: lambda runtime info
+    """
     db = get_db()
     setup_db(db)
 
 
 def get_db():
+    """ Create a database connection using SQLAlchemy.
+
+        :returns: hyp3_db module database object
+        :rtype: hyp3_db.Hyp3DB
+    """
     HOST = os.environ.get("Hyp3DBHost")
     USER = os.environ.get("Hyp3DBRootUser")
     PASS = os.environ.get("Hyp3DBRootPass")
