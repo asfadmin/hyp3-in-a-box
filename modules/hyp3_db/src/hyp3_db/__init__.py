64f4123eb7c58b55dd9e1fe616ac00c8cc95191b
Rohan Weeden <askaholic907@gmail.com>
2018-06-18 11:34:45 -0800

    Allow creating test_db with different db name

--- modules/hyp3_db/src/hyp3_db/__init__.py
+++ modules/hyp3_db/src/hyp3_db/__init__.py
@@ -2,11 +2,12 @@ from .hyp3_db import Hyp3DB
 from . import hyp3_models
 
 
-def make_test_db():
+def make_test_db(db="hyp3db"):
     return Hyp3DB(
         host="unit-testing.cxpvv5ynyjaw.us-west-2.rds.amazonaws.com",
         user="unittest",
-        password="unittestpass"
+        password="unittestpass",
+        db=db
     )
 
 
