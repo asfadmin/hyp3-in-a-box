c30d92c448ecb567fb2882bf9c6018328dd580ad
William Horn <wbhorn@alaska.edu>
2018-06-20 11:48:26 -0800

    Split event module into dispatch module

---
+++ lambdas/scheduler/src/dispatch/new_events.py
@@ -0,0 +1,17 @@
+from . import send
+
+import hyp3_events
+
+
+def new_events(new_hyp3_events):
+    print(f'Dispatching {len(new_hyp3_events)} events')
+
+    for event in new_hyp3_events:
+        dispatch_event(event)
+
+
+def dispatch_event(event):
+    if isinstance(event, hyp3_events.NotifyOnlyEvent):
+        send.notify_only(event)
+    else:
+        send.start_event(event)
