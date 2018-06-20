217ae0d30e598d3ce9dfad047f5e8cdd63c1f9e5
William Horn <wbhorn@alaska.edu>
2018-06-20 13:22:44 -0800

    Template for making sqs fifo queue

---
+++ cloudformation/tropo/templates/hyp3_sqs.py
@@ -0,0 +1,22 @@
+
+
+from troposphere import GetAtt
+from troposphere.sqs import Queue, RedrivePolicy
+
+from template import t
+
+print('  adding sqs')
+
+failed_start_evets = t.add_resource(Queue(
+    "FailedStartEvents",
+    FifoQueue=True
+))
+
+start_events = t.add_resource(Queue(
+    "Hyp3StartEvents",
+    FifoQueue=True,
+    RedrivePolicy=RedrivePolicy(
+        deadLetterTargetArn=GetAtt(failed_start_evets, "Arn"),
+        maxReceiveCount=1,
+    )
+))
