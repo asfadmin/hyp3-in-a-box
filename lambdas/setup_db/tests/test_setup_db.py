d0384e71158a8491a6c09fea8361b037f9eb9a70
William Horn <wbhorn@alaska.edu>
2018-06-21 15:16:34 -0800

    Don't use unittest class for test_setup_db

--- lambdas/setup_db/tests/test_setup_db.py
+++ lambdas/setup_db/tests/test_setup_db.py
@@ -5,23 +5,23 @@
 # Test cases for seting up the database
 
 
-from unittest import TestCase
-
 import import_setup_db
 
 import hyp3_db
 from init_db import setup_db
 
 
-class TestSetupDb(TestCase):
-    def setUp(self):
-        with hyp3_db.test_db(db='postgres') as admindb:
-            admindb.session.connection().connection.set_isolation_level(0)
-            admindb.session.execute("DROP DATABASE hyp3db;")
-            admindb.session.execute("CREATE DATABASE hyp3db;")
-            admindb.session.execute("DROP USER IF EXISTS hyp3_user;")
-            admindb.session.connection().connection.set_isolation_level(1)
+def test_lambda_function(self):
+    setup_db_for_test()
 
-    def test_lambda_function(self):
-        with hyp3_db.test_db() as db:
-            setup_db(db)
+    with hyp3_db.test_db() as db:
+        setup_db(db)
+
+
+def setup_db_for_test():
+    with hyp3_db.test_db(db='postgres') as admindb:
+        admindb.session.connection().connection.set_isolation_level(0)
+        admindb.session.execute("DROP DATABASE hyp3db;")
+        admindb.session.execute("CREATE DATABASE hyp3db;")
+        admindb.session.execute("DROP USER IF EXISTS hyp3_user;")
+        admindb.session.connection().connection.set_isolation_level(1)
