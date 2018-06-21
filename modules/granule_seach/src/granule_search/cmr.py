c54e3d367989dc12de17990847a3d167aca405a5
William Horn <wbhorn@alaska.edu>
2018-06-20 17:01:10 -0800

    Framework for seach_api and cmr

--- modules/granule_seach/src/granule_search/cmr.py
+++ modules/granule_seach/src/granule_search/cmr.py
@@ -2,4 +2,35 @@ from .search_api import GranuleSearchAPI
 
 
 class CMR(GranuleSearchAPI):
-    pass
+    """
+        Hoping to build a cleaner interface that will make interchanging
+        API's simple
+
+        Old:
+            api = CMRSearchAPI()
+
+            resp = api.query({
+                'provider': 'ASF',
+                'created_at[]': ["{},".format(prev_time)],
+                'platform[]': ['Sentinel-1A', 'Sentinel-1B'],
+                'page_size': MAX_RESULTS
+            })
+
+        New:
+            api = CMR()
+
+            resp = api \
+                .before(prev_time)
+                .limit(MAX_RESULTS)
+                .search()
+    """
+    @property
+    def api_url(self):
+        return "https://cmr.earthdata.nasa.gov/search/granules.json"
+
+    @staticmethod
+    def cmr_date_format(date):
+        return date.strftime('%Y-%m-%dT%H:%M:%SZ')
+
+    def search(self):
+        return NotImplemented
--- modules/granule_seach/src/granule_search/search_api.py
+++ modules/granule_seach/src/granule_search/search_api.py
@@ -1,2 +1,16 @@
-class GranuleSearchAPI:
-    pass
+import abc
+
+
+class GranuleSearchAPI(abc.ABC):
+    """ Class to wrap generic granule seach api"""
+
+    @property
+    @abc.abstractmethod
+    def api_url(self):
+        """Base url for the api"""
+        return NotImplemented
+
+    @abc.abstractmethod
+    def search(self):
+        """Build the query for the search api"""
+        return NotImplemented
