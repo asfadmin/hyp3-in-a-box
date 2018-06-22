1018f5012f3847c53c964a54d1ce66f48cf060fd
William Horn <wbhorn@alaska.edu>
2018-06-22 14:03:15 -0800

    Update get_new_granule_between in __init__ and tests

--- lambdas/find_new_granules/tests/test_find_new.py
+++ lambdas/find_new_granules/tests/test_find_new.py
@@ -17,9 +17,10 @@ find_new.environment.maturity = 'test'
     side_effect=custom_mocks.asf_api_requests_get
 )
 def test_get_new(mock_get):
-    prev_time = dt.datetime.now()
+    request_time = dt.datetime.now()
+    prev_time = request_time - dt.timedelta(minutes=5)
 
-    granules = find_new.get_new_granules_after(prev_time)
+    granules = find_new.get_new_granules_between(prev_time, request_time)
 
     assert isinstance(granules, list)
     assert all(isinstance(e, hyp3_events.NewGranuleEvent) for e in granules)
@@ -28,7 +29,7 @@ def test_get_new(mock_get):
 
 
 @mock.patch(
-    'find_new.find_new.get_new_granules_after',
+    'find_new.find_new.get_new_granules_between',
     side_effect=custom_mocks.asf_api_requests_get
 )
 @mock.patch(
