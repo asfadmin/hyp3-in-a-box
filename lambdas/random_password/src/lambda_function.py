aabe3935074d85df0b17b76dfd9d945030ab4408
William Horn <wbhorn@alaska.edu>
2018-06-26 15:18:35 -0800

    Random password lambda uses custom_resource module

--- lambdas/random_password/src/lambda_function.py
+++ lambdas/random_password/src/lambda_function.py
@@ -1,88 +1,11 @@
-import random
-import string
-import uuid
-import httplib
-import urlparse
 import json
+
+import random_password
 
 
 def lambda_handler(event, context):
     print(json.dumps(event))
-    response = CustomResource(event).get_response()
 
-    return send_response(event, response)
+    random_password.send_response(event)
 
-
-class CustomResource:
-    def __init__(self, event):
-        self.event = event
-
-    def get_response(self):
-        resource_id = self.event.get(
-            'PhysicalResourceId',
-            str(uuid.uuid4())
-        )
-
-        response = {
-            'StackId': self.event['StackId'],
-            'RequestId': self.event['RequestId'],
-            'LogicalResourceId': self.event['LogicalResourceId'],
-            'Status': 'SUCCESS',
-
-            'PhysicalResourceId': resource_id
-        }
-
-        if self.event['RequestType'] == 'Delete':
-            return response
-
-        try:
-            process_output = self.process()
-        except CustomResourceException as e:
-            response.update({
-                'Status': 'FAILED',
-                'Reason': str(e)
-            })
-
-        response.update(process_output)
-
-        return response
-
-    def process(self):
-        random_string = self.get_random_string()
-
-        return {
-            'Data': {'RandomString': random_string},
-            'Reason': 'Successfully generated a random string'
-        }
-
-    def get_random_string(self):
-        try:
-            length = int(self.event['ResourceProperties']['Length'])
-        except KeyError:
-            raise CustomResourceException('Must specify a length')
-        except ValueError:
-            raise CustomResourceException('Length not an integer')
-
-        characters = string.digits + string.ascii_letters
-
-        random_string = ''.join(
-            random.SystemRandom().choice(characters) for _ in range(length)
-        )
-
-        return random_string
-
-
-class CustomResourceException(Exception):
-    pass
-
-
-def send_response(request, response):
-    if not ('ResponseURL' in request or request['ResponseURL']):
-        return response
-
-    url = urlparse.urlparse(request['ResponseURL'])
-    body = json.dumps(response)
-    https = httplib.HTTPSConnection(url.hostname)
-    https.request('PUT', url.path+'?'+url.query, body)
-
-    return response
+    return 'SUCCESS'
--- lambdas/random_password/src/random_password.py
+++ lambdas/random_password/src/random_password.py
@@ -1,84 +1,46 @@
 import random
 import string
-import uuid
-import httplib
-import urlparse
-import json
 
-"""
-If included in a Cloudformation build as a CustomResource, generate a random
-string of length given by the 'length' parameter.
-By default the character set used is upper and lowercase ascii letters plus
-digits.  If the 'punctuation' parameter is specified this also includes
-punctuation. If you specify a KMS key ID then it will be encrypted, too.
-"""
+import custom_resource
 
 
-def lambda_handler(event, context):
-    response = {
-        'StackId': event['StackId'],
-        'RequestId': event['RequestId'],
-        'LogicalResourceId': event['LogicalResourceId'],
-        'Status': 'SUCCESS'
-    }
+def send_response(event):
+    response = RandomPassword(event) \
+        .get_response()
 
-    if 'PhysicalResourceId' in event:
-        response['PhysicalResourceId'] = event['PhysicalResourceId']
-    else:
-        response['PhysicalResourceId'] = str(uuid.uuid4())
+    return custom_resource.send(event, response)
 
-    if event['RequestType'] == 'Delete':
-        return send_response(event, response)
 
-    try:
-        length = int(event['ResourceProperties']['Length'])
-    except KeyError:
-        return send_response(
-            event,
-            response,
-            status='FAILED',
-            reason='Must specify a length'
-        )
-    except ValueError:
-        return send_response(
-            event,
-            response,
-            status='FAILED',
-            reason='Length not an integer'
+class RandomPassword(custom_resource.Base):
+    def _process(self):
+        random_password = self._get_random_string()
+
+        return {
+            'Data': {'Password': random_password},
+            'Reason': 'Successfully generated a random string'
+        }
+
+    def _get_random_string(self):
+        length = self._get_length()
+
+        characters = string.digits + string.ascii_letters
+
+        random_string = ''.join(
+            random.SystemRandom().choice(characters) for _ in range(length)
         )
 
-    try:
-        rds_compatible = event['ResourceProperties']['RDSCompatible']
-    except KeyError:
-        rds_compatible = False
+        return random_string
 
-    valid_characters = string.ascii_letters+string.digits
+    def _get_length(self):
+        try:
+            length = int(self.event['ResourceProperties']['Length'])
+        except KeyError:
+            raise custom_resource.CustomResourceException(
+                'Must specify a length'
+            )
+        except ValueError:
+            raise custom_resource.CustomResourceException(
+                'Length not an integer'
+            )
 
-    if rds_compatible not in [False, 'false', 'False']:
-        valid_characters = valid_characters.translate(None, '@/"')
-
-    random_string = ''.join(random.SystemRandom().choice(
-        string.digits + string.ascii_letters) for _ in range(length))
-
-    response['Data'] = {
-        'RandomString': random_string
-    }
-    response['Reason'] = 'Successfully generated a random string'
-
-    return send_response(event, response)
-
-
-def send_response(request, response, status=None, reason=None):
-    if status is not None:
-        response['Status'] = status
-
-    if reason is not None:
-        response['Reason'] = reason
-
-    if 'ResponseURL' in request and request['ResponseURL']:
-        url = urlparse.urlparse(request['ResponseURL'])
-        body = json.dumps(response)
-        https = httplib.HTTPSConnection(url.hostname)
-        https.request('PUT', url.path+'?'+url.query, body)
-
-    return response
+        return length
