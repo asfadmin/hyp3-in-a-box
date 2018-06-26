a633a49cf4f153ab7ab69ce6d9fab62b20edc7e3
William Horn <wbhorn@alaska.edu>
2018-06-26 13:05:50 -0800

    Refactor random_password custom resource function into class

--- lambdas/random_password/src/lambda_function.py
+++ lambdas/random_password/src/lambda_function.py
@@ -7,70 +7,82 @@ import json
 
 
 def lambda_handler(event, context):
-    response = {
-        'StackId': event['StackId'],
-        'RequestId': event['RequestId'],
-        'LogicalResourceId': event['LogicalResourceId'],
-        'Status': 'SUCCESS'
-    }
-
-    if 'PhysicalResourceId' in event:
-        response['PhysicalResourceId'] = event['PhysicalResourceId']
-    else:
-        response['PhysicalResourceId'] = str(uuid.uuid4())
-
-    if event['RequestType'] == 'Delete':
-        return send_response(event, response)
-
-    try:
-        length = int(event['ResourceProperties']['Length'])
-    except KeyError:
-        return send_response(
-            event,
-            response,
-            status='FAILED',
-            reason='Must specify a length'
-        )
-    except ValueError:
-        return send_response(
-            event,
-            response,
-            status='FAILED',
-            reason='Length not an integer'
-        )
-
-    try:
-        rds_compatible = event['ResourceProperties']['RDSCompatible']
-    except KeyError:
-        rds_compatible = False
-
-    valid_characters = string.ascii_letters+string.digits
-
-    if rds_compatible not in [False, 'false', 'False']:
-        valid_characters = valid_characters.translate(None, '@/"')
-
-    random_string = ''.join(random.SystemRandom().choice(
-        string.digits + string.ascii_letters) for _ in range(length))
-
-    response['Data'] = {
-        'RandomString': random_string
-    }
-    response['Reason'] = 'Successfully generated a random string'
+    print(json.dumps(event))
+    response = CustomResource(event).get_response()
 
     return send_response(event, response)
 
 
-def send_response(request, response, status=None, reason=None):
-    if status is not None:
-        response['Status'] = status
+class CustomResource:
+    def __init__(self, event):
+        self.event = event
 
-    if reason is not None:
-        response['Reason'] = reason
+    def get_response(self):
+        resource_id = self.event.get(
+            'PhysicalResourceId',
+            str(uuid.uuid4())
+        )
 
-    if 'ResponseURL' in request and request['ResponseURL']:
-        url = urlparse.urlparse(request['ResponseURL'])
-        body = json.dumps(response)
-        https = httplib.HTTPSConnection(url.hostname)
-        https.request('PUT', url.path+'?'+url.query, body)
+        response = {
+            'StackId': self.event['StackId'],
+            'RequestId': self.event['RequestId'],
+            'LogicalResourceId': self.event['LogicalResourceId'],
+            'Status': 'SUCCESS',
+
+            'PhysicalResourceId': resource_id
+        }
+
+        if self.event['RequestType'] == 'Delete':
+            return response
+
+        try:
+            process_output = self.process()
+        except CustomResourceException as e:
+            response.update({
+                'Status': 'FAILED',
+                'Reason': str(e)
+            })
+
+        response.update(process_output)
+
+        return response
+
+    def process(self):
+        random_string = self.get_random_string()
+
+        return {
+            'Data': {'RandomString': random_string},
+            'Reason': 'Successfully generated a random string'
+        }
+
+    def get_random_string(self):
+        try:
+            length = int(self.event['ResourceProperties']['Length'])
+        except KeyError:
+            raise CustomResourceException('Must specify a length')
+        except ValueError:
+            raise CustomResourceException('Length not an integer')
+
+        characters = string.digits + string.ascii_letters
+
+        random_string = ''.join(
+            random.SystemRandom().choice(characters) for _ in range(length)
+        )
+
+        return random_string
+
+
+class CustomResourceException(Exception):
+    pass
+
+
+def send_response(request, response):
+    if not ('ResponseURL' in request or request['ResponseURL']):
+        return response
+
+    url = urlparse.urlparse(request['ResponseURL'])
+    body = json.dumps(response)
+    https = httplib.HTTPSConnection(url.hostname)
+    https.request('PUT', url.path+'?'+url.query, body)
 
     return response
