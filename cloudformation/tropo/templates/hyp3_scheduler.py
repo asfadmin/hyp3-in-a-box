07c1cadf50a1b697e17a943e50821448cd7bcc3c
William Horn <wbhorn@alaska.edu>
2018-06-28 17:39:50 -0800

    Use units.make_lambda_function to reduce code duplication
    - Roles are passed in to keep the dependency inversion gods happy
    - Extra lambda params can be added using and argument
    - Lambda name template parameters conditionally added

--- cloudformation/tropo/templates/hyp3_scheduler.py
+++ cloudformation/tropo/templates/hyp3_scheduler.py
@@ -26,8 +26,8 @@ Resources
 
 from environment import environment
 from template import t
-from troposphere import GetAtt, Parameter, Ref
-from troposphere.awslambda import Environment, Function, VPCConfig
+from troposphere import GetAtt, Ref
+from troposphere.awslambda import Environment, VPCConfig
 from troposphere.iam import Policy, Role
 
 from . import utils
@@ -39,13 +39,6 @@ source_zip = "scheduler.zip"
 
 
 print('  adding scheduler lambda')
-
-lambda_name = t.add_parameter(Parameter(
-    "LambdaSchedulerName",
-    Description="Name of the Scheduler lambda function",
-    Default="hyp3_scheduler",
-    Type="String"
-))
 
 sns_policy = Policy(
     PolicyName='FinishEventSNSPublish',
@@ -71,34 +64,26 @@ lambda_role = t.add_resource(Role(
 ))
 
 
-scheduler_args = {
-    "FunctionName": Ref(lambda_name),
-    "Code": utils.make_lambda_code(
-        S3Bucket=environment.lambda_bucket,
-        S3Key="{maturity}/{source_zip}".format(
-            maturity=environment.maturity,
-            source_zip=source_zip
-        ),
-        S3ObjectVersion=environment.scheduler_version
-    ),
-    "Handler": "lambda_function.lambda_handler",
-    "Role": GetAtt(lambda_role, "Arn"),
-    "Runtime": "python3.6",
-    "Environment": Environment(
-        Variables={
-            'SNS_ARN': Ref(finish_sns),
-            'DB_HOST': utils.get_host_address(),
-            'DB_USER': Ref(db_user),
-            'DB_PASSWORD': Ref(db_pass),
-            'DB_NAME': Ref(db_name)
-        }),
-    "KmsKeyArn": GetAtt(kms_key, "Arn"),
-    "MemorySize": 128,
-    "Timeout": 300
-}
+scheduler = utils.make_lambda_function(
+    name="scheduler",
+    role=lambda_role,
+    lambda_params={
+        "Environment": Environment(
+            Variables={
+                'SNS_ARN': Ref(finish_sns),
+                'DB_HOST': utils.get_host_address(),
+                'DB_USER': Ref(db_user),
+                'DB_PASSWORD': Ref(db_pass),
+                'DB_NAME': Ref(db_name)
+            }),
+        "KmsKeyArn": GetAtt(kms_key, "Arn"),
+        "MemorySize": 128,
+        "Timeout": 300
+    }
+)
 
 if 'unittest' in environment.maturity:
-    scheduler_args['VpcConfig'] = VPCConfig(
+    scheduler.VpcConfig = VPCConfig(
         SecurityGroupIds=[
             'sg-0d8cdb7c',
             'sg-72f8c803'
@@ -110,7 +95,4 @@ if 'unittest' in environment.maturity:
         ]
     )
 
-scheduler = t.add_resource(Function(
-    "SchedulerFunction",
-    **scheduler_args
-))
+t.add_resource(scheduler)
