76464473d0a2fcdaf3a03b93a5f4a4f56b2c0a31
William Horn <wbhorn@alaska.edu>
2018-07-09 10:44:13 -0800

    Simplify natural earth url storage
    Just use strings instead of keeping them in a class

--- processes/notify_only/src/natural_earth.py
+++ processes/notify_only/src/natural_earth.py
@@ -5,66 +5,42 @@ import requests
 import notify_only_utils as utils
 
 
-class NaturalEarthUrl:
-    def __init__(self, geom_name, zip_name, data_classification, scale):
-        self.base_download_url = (
-            'https://www.naturalearthdata.com/http/'
-            '/www.naturalearthdata.com/download'
-        )
-
-        self.geom_name = geom_name
-        self.scale = scale
-        self.zip_name = zip_name
-        self.data_classification = data_classification
-
-    def build_url(self):
-        return '{base}/{scale}/{data}/{zip_name}'.format(
-            base=self.base_download_url,
-            scale=self.scale,
-            data=self.data_classification,
-            zip_name=self.zip_name
-        )
-
-
 def download(geom_name):
-    ne_download = get_download_from(geom_name)
+    natural_earth_download_url = get_url_for(geom_name)
     download_path_str = f'data/{geom_name}/{geom_name}.zip'
 
-    return get_shapefile(download_path_str, ne_download)
+    return get_shapefile(
+        download_path_str,
+        natural_earth_download_url
+    )
 
 
-def get_download_from(geom):
+def get_url_for(geom):
     return {
-        'oceans': NaturalEarthUrl(
-            geom_name='oceans',
-            scale='10m',
-            data_classification='physical',
-            zip_name='ne_10m_ocean.zip'
+        "countries": (
+            "https://www.naturalearthdata.com/http/"
+            "/www.naturalearthdata.com/download/10m/cultural/"
+            "ne_10m_admin_0_countries.zip"
         ),
-        'countries': NaturalEarthUrl(
-            geom_name='countries',
-            scale='10m',
-            data_classification='cultural',
-            zip_name='ne_10m_admin_0_countries.zip'
+        "oceans": (
+            "https://www.naturalearthdata.com/http/"
+            "/www.naturalearthdata.com/download/10m/physical/"
+            "ne_10m_ocean.zip"
         ),
-        'lakes': NaturalEarthUrl(
-            geom_name='lakes',
-            scale='10m',
-            data_classification='physical',
-            zip_name='ne_10m_lakes.zip'
+        "lakes":  (
+            "https://www.naturalearthdata.com/http/"
+            "/www.naturalearthdata.com/download/10m/physical/"
+            "ne_10m_lakes.zip"
         )
     }[geom]
 
 
-def get_shapefile(path_str, ne_download):
+def get_shapefile(path_str, natural_earth_download_url):
     print('starting download')
     path = utils.get_base_path() / path_str
-
     path.parent.mkdir(parents=True, exist_ok=True)
 
-    url = ne_download.build_url()
-    dl_path = download_file(url, path)
-
+    dl_path = download_file(natural_earth_download_url, path)
     unzip(dl_path)
 
     return get_shapefile_in(dl_path.parent)
