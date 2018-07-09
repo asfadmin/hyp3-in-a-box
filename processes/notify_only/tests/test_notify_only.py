c85b14ecc08b090fd74eaa4e2def524d9a1ffba2
William Horn <wbhorn@alaska.edu>
2018-07-09 12:20:12 -0800

    Added another granule polygon to notify only tests

--- processes/notify_only/tests/test_notify_only.py
+++ processes/notify_only/tests/test_notify_only.py
@@ -45,10 +45,10 @@ def get_shapefile_path(dl_path):
     'natural_earth.download',
     side_effect=get_shapefile_path)
 def test_notify_only_browse(download_mock):
-    gran_poly = (
+    gran_polys = [(
         'POLYGON((30.455441 18.597095,30.770906 20.107595,28.401737 '
         '20.530865,28.10887 19.023712,30.455441 18.597095))'
-    )
+    ), 'POLYGON((-122.374016 40.047749,-121.967674 38.427338,-119.047066 38.829082,-119.382103 40.448418,-122.374016 40.047749))']
     subscription_poly = (
         'POLYGON((24.2041015625 26.37285889825242,33.4326171875 '
         '28.633406561208858,41.0791015625 18.917390875830886,'
@@ -56,7 +56,8 @@ def test_notify_only_browse(download_mock):
         '19.94966746671995,24.2041015625 26.37285889825242))'
     )
 
-    png_path = notify_only.browse(gran_poly, subscription_poly)
+    for gran_poly in gran_polys:
+        png_path = notify_only.browse(gran_poly, subscription_poly)
 
-    assert isinstance(png_path, str)
-    assert png_path.endswith('.png')
+        assert isinstance(png_path, str)
+        assert png_path.endswith('.png')
