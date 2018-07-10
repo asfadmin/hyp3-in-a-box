d1a57d75d9776868883fce807dd43cc359bd6d30
William Horn <wbhorn@alaska.edu>
2018-07-10 13:03:47 -0800

    Test code for generating an api key
    Mainly this is just some example code

--- modules/hyp3_db/tests/test_hyp3_db.py
+++ modules/hyp3_db/tests/test_hyp3_db.py
@@ -1,6 +1,27 @@
 import hyp3_db_test_utils as tu
 
+from hyp3_db import hyp3_models
+
 
 @tu.with_db
-def test_db_creation(db):
-    assert db is not None
+def test_api_key_generation(db):
+    user = hyp3_models.User(
+        username="test",
+        email="test@email.com",
+        is_admin=True,
+        is_authorized=True,
+        granules_processed=0
+    )
+
+    db.session.add(user)
+    update_db_with(db, user)
+
+    key, api_key = hyp3_models.ApiKey.generate_new(user.id)
+    db.session.add(api_key)
+
+    print(f"key: {key} \n hash: {api_key.hash}")
+
+
+def update_db_with(db, obj):
+    db.session.flush()
+    db.session.refresh(obj)
