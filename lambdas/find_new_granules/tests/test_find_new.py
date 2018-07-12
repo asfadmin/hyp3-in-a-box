ff109fe607ee52475614c57e6c93a38a4088d03b
William Horn <wbhorn@alaska.edu>
2018-07-11 18:06:06 -0800

    Add mocks back into find_new test

--- lambdas/find_new_granules/tests/test_find_new.py
+++ lambdas/find_new_granules/tests/test_find_new.py
@@ -28,5 +28,15 @@ def test_get_new(mock_get):
     mock_get.assert_called()
 
 
-def test_s3_upload():
+@mock.patch(
+    'find_new.find_new.get_new_granules_between',
+    side_effect=find_new_mocks.asf_api_requests_get
+)
+@mock.patch(
+    'find_new.s3.upload'
+)
+def test_s3_upload(mock_s3_upload, mock_find_new):
     find_new.granule_events()
+
+    mock_s3_upload.assert_called_once()
+    mock_find_new.assert_called_once()
