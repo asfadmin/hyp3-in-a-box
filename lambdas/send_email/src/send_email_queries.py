898f363f01b7386bbc766ddffa5a9e89d254d14e
William Horn <wbhorn@alaska.edu>
2018-07-24 13:25:00 -0800

    [HIB-81] Refactor db queries into seprate file
    This is so mocking them out is easier

---
+++ lambdas/send_email/src/send_email_queries.py
@@ -0,0 +1,19 @@
+from hyp3_db.hyp3_models import User, OneTimeAction
+
+
+def get_user_by_email(db, email):
+    return db.session.query(User) \
+        .filter_by(email=email) \
+        .first()
+
+
+def get_unsub_action(db, user_id):
+    return db.session.query(OneTimeAction) \
+        .filter_by(user_id=user_id) \
+        .filter_by(action='unsubscribe') \
+        .first()
+
+
+def add_action(db, action):
+    db.session.add(action)
+    db.session.commit()
