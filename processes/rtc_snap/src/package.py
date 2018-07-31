4558c94e9c54b9b339445da83153c17424f2b9ec
William Horn <wbhorn@alaska.edu>
2018-07-30 16:01:20 -0800

    [HIB-102] Packaging processing output files from list of globs

--- processes/rtc_snap/src/package.py
+++ processes/rtc_snap/src/package.py
@@ -1,11 +1,26 @@
+
+from ordered_set import OrderedSet
+
+import pathlib as pl
 import zipfile as zf
 
 
 def outputs_from(working_dir, file_patterns):
+    work_dir_path = pl.Path(working_dir)
     zip_name = 'output.zip'
 
+    output_file_paths = []
+    for pattern in file_patterns:
+        output_file_paths += list(work_dir_path.glob(pattern))
+
     with zf.ZipFile(zip_name, 'w') as output_zip:
-        output_zip.write(working_dir + '/hello.txt')
+        for output_file_path in output_file_paths:
+            arc_path_name_parts = \
+                OrderedSet(work_dir_path.parts).symmetric_difference(
+                    OrderedSet(output_file_path.parts)
+                )
+
+            arc_path = pl.Path(*list(arc_path_name_parts))
+            output_zip.write(output_file_path, arcname=arc_path)
 
     return zip_name
-
