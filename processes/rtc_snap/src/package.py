85a7e03865a96bbba177407036fe1a0f605cd7be
William Horn <wbhorn@alaska.edu>
2018-07-31 08:36:46 -0800

    [HIB-102] More refactoring in rtc snap packaging

--- processes/rtc_snap/src/package.py
+++ processes/rtc_snap/src/package.py
@@ -7,7 +7,7 @@ from ordered_set import OrderedSet
 def outputs_from(zip_name, working_dir, file_patterns):
     work_dir_path = pl.Path(working_dir)
 
-    output_file_paths = find_output_file_paths(
+    output_file_paths = find_output_files(
         work_dir_path,
         file_patterns
     )
@@ -17,37 +17,51 @@ def outputs_from(zip_name, working_dir, file_patterns):
         work_dir_path
     )
 
+    return create_archive(zip_name, output_file_paths, archive_paths)
+
+
+def find_output_files(work_dir_path, file_patterns):
+    files_matching_pattern = [
+        find_files_matching(pattern, work_dir_path)
+        for pattern in file_patterns
+    ]
+
+    return sum(files_matching_pattern, [])
+
+
+def find_files_matching(pattern, work_dir_path):
+    return list(work_dir_path.glob(pattern))
+
+
+def remove_working_dir_from(output_file_paths, work_dir_path):
+    return [
+        remove_working_dir_from_path(output_file_path, work_dir_path)
+        for output_file_path in output_file_paths
+    ]
+
+
+def remove_working_dir_from_path(output_file_path, work_dir_path):
+    work_dir_set, output_file_set = [
+        make_ordered_set_from(path)
+        for path in [work_dir_path, output_file_path]
+    ]
+
+    path_without_working_dir = work_dir_set ^ output_file_set
+
+    return pl.Path(*path_without_working_dir)
+
+
+def make_ordered_set_from(path):
+    return OrderedSet(path.parts)
+
+
+def create_archive(zip_name, output_paths, archive_paths):
     with zf.ZipFile(zip_name, 'w') as archive:
-        add_files_to(archive, output_file_paths, archive_paths)
+        add_files_to(archive, zip(output_paths, archive_paths))
 
     return zip_name
 
 
-def find_output_file_paths(work_dir_path, file_patterns):
-    output_file_paths = []
-
-    for pattern in file_patterns:
-        output_file_paths += list(work_dir_path.glob(pattern))
-
-    return output_file_paths
-
-
-def remove_working_dir_from(output_file_paths, work_dir_path):
-    archive_paths = []
-
-    for output_file_path in output_file_paths:
-        arc_path_name_parts = \
-            OrderedSet(work_dir_path.parts).symmetric_difference(
-                OrderedSet(output_file_path.parts)
-            )
-
-        arc_path = pl.Path(*list(arc_path_name_parts))
-
-        archive_paths.append(arc_path)
-
-    return archive_paths
-
-
-def add_files_to(archive, output_paths, archive_paths):
-    for output_path, archive_path in zip(output_paths, archive_paths):
+def add_files_to(archive, output_files):
+    for output_path, archive_path in output_files:
         archive.write(output_path, arcname=archive_path)
