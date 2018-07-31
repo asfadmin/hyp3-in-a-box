5bb22faf89190663d88c03bdf6c8c7cf4c9d184d
William Horn <wbhorn@alaska.edu>
2018-07-30 16:02:04 -0800

    [HIB-102] Tests basic packaging case as well as rtc-snap example case

---
+++ processes/rtc_snap/test/rtc_snap_strategies.py
@@ -0,0 +1,20 @@
+from hypothesis import strategies as st
+
+import asf_granule_util as gu
+
+
+def granules():
+    return st.from_regex(gu.SentinelGranule.pattern_exact)
+
+
+def rtc_example_files():
+    g = 'S1B_IW_GRDH_1SDV_20170731T100601_20170731T100626_006730_00BD74_1D4A'
+    base = f'{g}-PREDORB-30m-rtc-s1tbx'
+
+    return [
+        f"{base}/ESA_citation.txt",
+        f"{base}/{g}_OB_CAL_SF_ML_TF_TC_GVH.tif",
+        f"{base}/{g}_OB_CAL_SF_ML_TF_TC_GVV_large.png",
+        f"{base}/{g}_OB_CAL_SF_ML_TF_TC_GVV.png",
+        f"{base}/{g}_OB_CAL_SF_ML_TF_TC_GVV.tif"
+    ]
---
+++ processes/rtc_snap/test/test_package.py
@@ -0,0 +1,63 @@
+import pathlib as pl
+import zipfile as zf
+import pytest
+
+import rtc_snap_strategies as strats
+
+import import_rtc_snap
+import package
+
+package_test_cases = [(
+    ['granule/hello.txt', 'test2.txt', 'test.png'],
+    ['*/*.txt', '*.png'],
+    ['granule/hello.txt', 'test.png']
+), (
+    ['hello.txt', 'test.png'],
+    ['*/*.txt'],
+    []
+), (
+    strats.rtc_example_files(),
+    ["*/*_TC_G??.tif", "*/*.png", "*/*.txt"],
+    strats.rtc_example_files()
+)]
+
+
+@pytest.mark.parametrize(
+    "file_paths,patterns,expected",
+    package_test_cases
+)
+def test_package(tmpdir, file_paths, patterns, expected):
+    working_dir = tmpdir.mkdir('product-outputs')
+    create_output_files(
+        file_paths,
+        working_dir
+    )
+
+    output_zip = package.outputs_from(
+        str(working_dir),
+        patterns
+    )
+
+    assert output_zip.endswith('.zip')
+    assert zf.is_zipfile(output_zip)
+
+    with zf.ZipFile(output_zip, 'r') as z:
+        assert set(z.namelist()) == set(expected)
+
+
+def create_output_files(output_paths, working_dir):
+    for p in output_paths:
+        full_path = pl.Path(working_dir) / p
+
+        write_sample_file(full_path)
+
+
+def write_sample_file(path):
+    path.parent.mkdir(parents=True, exist_ok=True)
+    with path.open('w') as f:
+        f.write('test')
+
+
+@pytest.fixture()
+def output_patterns():
+    return
