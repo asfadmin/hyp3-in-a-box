7ca060762c841b34fd0af0bee070f586f6021484
William Horn <wbhorn@alaska.edu>
2018-08-03 12:18:06 -0800

    [HIB-121] Add setup.py hyp3_process module

---
+++ modules/hyp3_process/setup.py
@@ -0,0 +1,20 @@
+import codecs
+
+from setuptools import find_packages, setup
+
+with codecs.open('requirements.txt', 'r') as f:
+    requirements = f.read().strip().split('\n')
+
+    if '-i https://pypi.org/simple' in requirements[0]:
+        requirements = requirements[1:]
+
+setup(
+    name='Hyp3 Process',
+    version='0.1dev',
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    license="GPLv3+",
+    description=("Module for wrapping hyp3 functionality"
+                 " around processing scripts"),
+    install_requires=requirements
+)
