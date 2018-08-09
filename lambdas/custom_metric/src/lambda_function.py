7f0565fbc5c0709b4950a013f2a07af500b1c149
Rohan Weeden <askaholic907@gmail.com>
2018-08-09 13:35:41 -0800

    Refactored

--- lambdas/custom_metric/src/lambda_function.py
+++ lambdas/custom_metric/src/lambda_function.py
@@ -14,43 +14,17 @@ def lambda_handler(event, context):
     queue_url = event['QueueUrl']
     ag_name = event['AutoScalingGroupName']
 
-    resp = sqs_client.get_queue_attributes(
-        QueueUrl=queue_url,
-        AttributeNames=[
-            'ApproximateNumberOfMessages'
-        ]
-    )
-    num_messages = int(resp['Attributes']['ApproximateNumberOfMessages'])
-
-    # TODO: Paginate?
-    response = asg_client.describe_auto_scaling_groups(
-        AutoScalingGroupNames=[
-            ag_name,
-        ],
-        MaxRecords=100
-    )
-
-    num_instances = len(
-        list(
-            filter(
-                lambda x: x['LifecycleState'] == 'InService',
-                response['AutoScalingGroups'][0]['Instances']
-            )
-        )
-    )
+    num_messages = get_num_messages(queue_url)
+    num_instances = get_num_instances(ag_name)
 
     print("Number of messages: ", num_messages)
     print("Number of running instances: ", num_instances)
 
-    if num_instances == 0:
-        # Something kindof arbitrary, but still greater than having 1 instance
-        messages_per_instance = num_messages * 2
-    else:
-        messages_per_instance = num_messages / num_instances
+    messages_per_instance = calculate_metric(num_messages, num_instances)
 
     print("Current number of messages per instance: ", messages_per_instance)
 
-    response = cw_client.put_metric_data(
+    cw_client.put_metric_data(
         Namespace=STACK_NAME,
         MetricData=[
             {
@@ -59,3 +33,42 @@ def lambda_handler(event, context):
             },
         ]
     )
+
+
+def get_num_messages(queue_url):
+    resp = sqs_client.get_queue_attributes(
+        QueueUrl=queue_url,
+        AttributeNames=[
+            'ApproximateNumberOfMessages'
+        ]
+    )
+    return int(resp['Attributes']['ApproximateNumberOfMessages'])
+
+
+def get_num_instances(group_name):
+    # TODO: Paginate?
+    response = asg_client.describe_auto_scaling_groups(
+        AutoScalingGroupNames=[
+            group_name,
+        ],
+        MaxRecords=100
+    )
+
+    return len(filter_active_only(response['AutoScalingGroups'][0]['Instances']))
+
+
+def filter_active_only(instances):
+    return list(
+        filter(
+            lambda x: x['LifecycleState'] == 'InService',
+            instances
+        )
+    )
+
+
+def calculate_metric(num_messages, num_instances):
+    if num_instances == 0:
+        # Something kindof arbitrary, but still greater than having 1 instance
+        return num_messages * 2
+
+    return num_messages / num_instances
