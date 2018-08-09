b66a3b6eebcea35698c0ef3e626cd81bd107b9d8
William Horn <wbhorn@alaska.edu>
2018-08-09 19:53:37 +0000

    load creds from file

--- ec2/worker/src/hyp3_handler.py
+++ ec2/worker/src/hyp3_handler.py
@@ -1,4 +1,5 @@
 import subprocess
+import json
 
 import asf_granule_util as gu
 
@@ -31,6 +32,6 @@ if __name__ == "__main__":
     handler(
         'S1A_IW_SLC__1SDV_20150325T020716_20150325T020746_005182_00689D_9D9B',
         '/home/ubuntu/jobs/test-wrk-dir',
-        {"username": "hyp3_asf", "password": "8dppP7+?r<z=2jg42h"},
+        json.load(open('earthdata_creds.json', 'r')),
         '/home/ubuntu/hyp3-in-a-box/processes/rtc_snap/build/hyp3-rtc-snap/src/procSentinelRTC-3.py'
     )
