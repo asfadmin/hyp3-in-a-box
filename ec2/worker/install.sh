09c6a866d702e94872d908b8cb089d4cd27e6934
William Horn <wbhorn@alaska.edu>
2018-08-10 19:51:46 +0000

    [HIB-118] Copy rtc_snap hyp3_handler to ec2/worker in install.sh

--- ec2/worker/install.sh
+++ ec2/worker/install.sh
@@ -5,5 +5,9 @@ python3.6 -m pip install -e \
 python3.6 -m pip install -e \
     $REPO_PATH/modules/hyp3_process
 
+cp \
+    $REPO_PATH/processes/rtc_snap/src/hyp3_handler.py \
+    $REPO_PATH/ec2/worker/src/.
+
 cp $REPO_PATH/ec2/worker/hyp3.service /etc/systemd/system/ 
-cp $REPO_PATH/ec2/worker/src/hyp3_daemon.py /opt/hyp3/
+cp $REPO_PATH/ec2/worker/src/* /opt/hyp3/*
