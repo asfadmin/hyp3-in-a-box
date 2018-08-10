ef534659c616217e36f0039dd2f92152f6097b27
Rohan Weeden <askaholic907@gmail.com>
2018-08-10 08:20:46 -0800

    Dedent multiline strings.

--- cloudformation/tropo/templates/ec2_userdata.py
+++ cloudformation/tropo/templates/ec2_userdata.py
@@ -1,19 +1,21 @@
-from troposphere import Base64, Ref, Sub
+from textwrap import dedent
 
 from tropo_env import environment
+from troposphere import Base64, Ref, Sub
 
 
 def make_userdata_from_environment():
-    return """#! /bin/bash
-echo STACK_NAME=${{StackName}} > ~/env
+    return dedent("""
+        #! /bin/bash
+        echo STACK_NAME=${{StackName}} > ~/env
 
-{PullCode}
+        {PullCode}
 
-systemctl restart hyp3
-    """.format(
-        PullCode=get_hyp3_daemon_install_script() if environment.clone_in_userdata
-        else ""
-    )
+        systemctl restart hyp3
+        """.format(
+            PullCode=get_hyp3_daemon_install_script() if environment.clone_in_userdata
+            else ""
+        ))
 
 
 def get_hyp3_daemon_install_script():
@@ -21,20 +23,20 @@ def get_hyp3_daemon_install_script():
     create a new AMI when the orchestration code changes, just let the AMI pull
     the new version of the code on startup."""
 
-    return """
-CLONE_TOKEN=$(aws ssm get-parameters --names /CodeBuild/GITHUB_HYP3_API_CLONE_TOKEN --output text --with-decryption | awk {'print $4'})
-cd /tmp
-git clone --single-branch -b dev https://$CLONE_TOKEN@github.com/asfadmin/hyp3-in-a-box --depth=1
-cd hyp3-in-a-box/ec2/worker
+    return dedent("""
+        CLONE_TOKEN=$(aws ssm get-parameters --names /CodeBuild/GITHUB_HYP3_API_CLONE_TOKEN --output text --with-decryption | awk {'print $4'})
+        cd /tmp
+        git clone --single-branch -b dev https://$CLONE_TOKEN@github.com/asfadmin/hyp3-in-a-box --depth=1
+        cd hyp3-in-a-box/ec2/worker
 
-cp src/hyp3_daemon.py /opt/hyp3/
-cp hyp3.service /etc/systemd/system/
-pip install -r requirements.txt
+        cp src/hyp3_daemon.py /opt/hyp3/
+        cp hyp3.service /etc/systemd/system/
+        pip install -r requirements.txt
 
-cd /tmp/hyp3-in-a-box/processes/rtc_snap
-cp src/hyp3_handler.py /opt/hyp3/
-pip install -r requirements.txt
-"""
+        cd /tmp/hyp3-in-a-box/processes/rtc_snap
+        cp src/hyp3_handler.py /opt/hyp3/
+        pip install -r requirements.txt
+        """)
 
 
 user_data = Base64(
