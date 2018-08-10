cb79f92532244a1ecf541eb52fda64775ce68841
William Horn <wbhorn@alaska.edu>
2018-08-10 20:27:09 +0000

    [HIB-118] Dummy hyp3_handler.py in ec2/worker for testing

--- ec2/worker/src/hyp3_handler.py
+++ ec2/worker/src/hyp3_handler.py
@@ -1,8 +1,5 @@
+import pathlib as pl
 from typing import Dict
-import json
-import subprocess
-
-import asf_granule_util as gu
 
 
 def handler(
@@ -11,30 +8,8 @@ def handler(
     earthdata_creds: Dict[str, str],
     script_path: str
 ) -> None:
-    print('processing rtc product')
-
-    gu.download(
-        granule_name,
-        earthdata_creds,
-        directory=working_dir,
-        progress_bar=True
-
-    )
-
-    subprocess.check_call([
-        'python2', script_path,
-        '--ls',
-        '-r', '30',
-        f'{granule_name}.zip'
-    ],
-        cwd=working_dir
-    )
-
-
-if __name__ == "__main__":
-    handler(
-        "S1A_IW_GRDH_1SDV_20150329T013253_20150329T013318_005240_0069E3_0AE5",
-        "/home/ubuntu/jobs/test",
-        json.load(open('creds.json', 'r')),
-        '/home/ubuntu/hyp3-in-a-box/processes/rtc_snap/build/hyp3-rtc-snap/src/procSentinelRTC-3.py'
-    )
+    files = ['hello.txt', 'browse.png']
+    
+    for f in files:
+        with (pl.Path(working_dir) / f).open('w') as f:
+            f.write('test file')
