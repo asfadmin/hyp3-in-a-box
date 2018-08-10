68b5f6b1b75321cd75ce59cbe0b6d5740f5d9d93
William Horn <wbhorn@alaska.edu>
2018-08-10 12:33:29 -0800

    [HIB-118] Shorten lines that are too long in sample start event

--- modules/hyp3_events/src/start-event-rtc-example.py
+++ modules/hyp3_events/src/start-event-rtc-example.py
@@ -9,14 +9,16 @@ def rand(N):
 
 
 se = hyp3_events.StartEvent(
-    granule='S1A_IW_GRDH_1SDV_20150329T013228_20150329T013253_005240_0069E3_95F9',
+    granule=('S1A_IW_GRDH_1SDV_20150329T013228'
+             '_20150329T013253_005240_0069E3_95F9'),
     user_id=1,
     sub_id=1,
     output_patterns={
         "archive": ["*/*_TC_G??.tif", "*/*.png", "*/*.txt"],
         "browse": "*/*.png"
     },
-    script_path='/home/ubuntu/hyp3-in-a-box/processes/rtc_snap/build/hyp3-rtc-snap/src/procSentinelRTC-3.py',
+    script_path=('/home/ubuntu/hyp3-in-a-box/processes/rtc_snap/build/'
+                 'hyp3-rtc-snap/src/procSentinelRTC-3.py'),
     additional_info=[{
         'name': 'test',
         'value': rand(5)
