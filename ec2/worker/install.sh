d3cd42eec515c2fa5677dec6b8acbe9b046178df
William Horn <wbhorn@alaska.edu>
2018-08-14 16:43:44 -0800

    Use relative paths in ec2/worker install script

--- ec2/worker/install.sh
+++ ec2/worker/install.sh
@@ -1,9 +1,9 @@
-REPO_PATH=/home/ubuntu/hyp3-in-a-box
+pushd ../..
 
 python3.6 -m pip install -e \
-    $REPO_PATH/modules/hyp3_events
+    ./modules/hyp3_events
 python3.6 -m pip install -e \
-    $REPO_PATH/modules/hyp3_process
+    ./modules/hyp3_process
 
 
 if [ "$1" = "dummy" ]
@@ -19,24 +19,14 @@ else
     HANDLER_PATH=./processes/rtc_snap/src/hyp3_handler.py
 fi
 
-
 cp \
     $HANDLER_PATH
     ./ec2/worker/src/hyp3_handler.py
 
+cp ./ec2/worker/hyp3.service /etc/systemd/system/
 
-if [ -n "$RTC_SNAP_HANDLER" ]
-then
-    echo "Using rtc handler function"
-    cp \
-        $REPO_PATH/processes/rtc_snap/src/hyp3_handler.py \
-        $REPO_PATH/ec2/worker/src/
-else
-    echo "Using dummy handler function"
-fi
-
-cp $REPO_PATH/ec2/worker/hyp3.service /etc/systemd/system/
-
-cp $REPO_PATH/ec2/worker/src/hyp3_handler.py /opt/hyp3/
-cp $REPO_PATH/ec2/worker/src/hyp3_daemon.py /opt/hyp3/
+cp ./ec2/worker/src/hyp3_handler.py /opt/hyp3/
+cp ./ec2/worker/src/hyp3_daemon.py /opt/hyp3/
 chmod +x /opt/hyp3/hyp3_daemon.py
+
+popd
