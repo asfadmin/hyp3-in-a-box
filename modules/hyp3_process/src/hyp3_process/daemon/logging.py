85a32817aada16ffade48b3abc1dfc67b1ecf514
Rohan Weeden <askaholic907@gmail.com>
2018-08-17 15:06:45 -0800

    Set logging level so that debug messages gets written to the file

--- modules/hyp3_process/src/hyp3_process/daemon/logging.py
+++ modules/hyp3_process/src/hyp3_process/daemon/logging.py
@@ -10,10 +10,10 @@ import logging
 def getLogger(name):
     logger = logging.getLogger(name)
 
-    logger.setLevel(logging.INFO)
+    logger.setLevel(logging.DEBUG)
 
     console_handler = logging.StreamHandler()
-    console_handler.setLevel(logging.DEBUG)
+    console_handler.setLevel(logging.INFO)
     file_handler = logging.FileHandler("/var/log/hyp3.log")
     file_handler.setLevel(logging.DEBUG)
 
