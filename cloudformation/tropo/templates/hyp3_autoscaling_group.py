ac6a443d3354f3e2ef51834977b8e3bdec6dcbbb
Rohan Weeden <askaholic907@gmail.com>
2018-08-17 16:59:07 -0800

    Describe the scaling policy

--- cloudformation/tropo/templates/hyp3_autoscaling_group.py
+++ cloudformation/tropo/templates/hyp3_autoscaling_group.py
@@ -7,9 +7,17 @@ Troposphere template responsible for creating the worker auto scaling group.
 This group adds instances as more requests are added to the processing SQS
 Queue.
 
+Scaling is done through a TargetTracking policy based on a custom metric. The
+custom metric is simply the ratio of messages available in the StartEvents
+Queue to active instances in the autoscaling group. The target value is the
+ratio of messages to instances that the autoscaling group should try to
+maintain. For example a target of 1 means that for each new message, the group
+will attempt to start a new processing instance (provided it is not at max
+capacity yet).
+
 Processing instances are configured through the userdata. The CloudFormation
 adds the stack name to the userdata string, which sets the value as an
-environment variable before restarting the hyp3 daemon service. All other
+environment variable before restarting the HyP3 daemon service. All other
 general configuration variables (e.g. queue name, sns arn, products bucket) are
 stored in SSM Parameter Store and read by the instance on startup. The names of
 these parameters are known ahead of time except for the stack name prefix,
@@ -17,7 +25,7 @@ which is supplied by the user data.
 
 For the purposes of development, the :ref:`userdata_helper` will checkout the
 latest orchestration code from the hyp3-in-a-box dev branch before starting the
-hyp3 daemon. This makes testing changes a lot easier because it means no new AMI
+HyP3 daemon. This makes testing changes a lot easier because it means no new AMI
 is required, and no manual copying of files is needed. This "development mode"
 can be enabled by setting the ``clone_in_userdata`` environment variable to
 ``True`` when generating the CloudFormation template.
@@ -33,7 +41,7 @@ Requires
 Resources
 ~~~~~~~~~
 
-* **Auto Scaling Group:** The cluster of hyp3 processing instances.
+* **Auto Scaling Group:** The cluster of HyP3 processing instances.
 * **Launch Configuration:** Instance definitions for the auto scaling group.
 * **Security Group:** Firewall rules for processing instances.
 * **Cloudwatch Alarm:** Created by the TargetTrackingScaling Policy.
