f54c1f9f21df260a929c6c50188fa9e5e6265b8b
Rohan Weeden <askaholic907@gmail.com>
2018-08-23 11:05:32 -0800

    Update spot price param description

--- cloudformation/tropo/templates/hyp3_autoscaling_group.py
+++ cloudformation/tropo/templates/hyp3_autoscaling_group.py
@@ -108,8 +108,7 @@ instance_type = t.add_parameter(Parameter(
 
 spot_price = t.add_parameter(Parameter(
     "RTCProcessingSpotPrice",
-    Description="The maximum price to pay for a spot instance. \
-    Setting this value enables spot processing.",
+    Description="The maximum price to pay for a spot instance (required).",
     Type="Number"
 ))
 
