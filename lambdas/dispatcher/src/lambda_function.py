7d3a68241694b69167941cd5827f12e389ea4da9
mnamneus <mnamneus@alaska.edu>
2018-09-28 09:53:22 -0800

    moved required environment data from scheduler

--- lambdas/dispatcher/src/lambda_function.py
+++ lambdas/dispatcher/src/lambda_function.py
@@ -1,5 +1,8 @@
 from typing import List, Dict, Any
 import json
+import os.environ
+
+from . import dispatch, environment
 
 
 
@@ -7,9 +10,15 @@ import json
 def lambda_handler(events: List[Dict[str: Any]]) -> None:
     """
 
-    :param event: data for the job from the scheduler
+    :param events: data for the StartEvents from the scheduler
     """
 
     print(json.dumps(events))
-
+    setup_environment()
     dispatch(events)
+
+
+def setup_environment():
+
+    environment.sns_arn = os.environ['SNS_ARN']
+    environment.queue_url = os.environ['QUEUE_URL']
