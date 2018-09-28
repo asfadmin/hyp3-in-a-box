07b5d513b4a566d8f45e956e007787903e3a6d05
mnamneus <mnamneus@alaska.edu>
2018-09-28 09:56:26 -0800

    removed imports of code moved to dispatcher

--- lambdas/scheduler/src/events/__init__.py
+++ lambdas/scheduler/src/events/__init__.py
@@ -3,6 +3,5 @@ This module is to give communication between the hyp3 system functions some
 amount of type safety.
 """
 
-from .make import make_from, make_new_granule_events_with
+from .make import make_new_granule_events_with
 
-__all__ = ['make_from', 'make_new_granule_events_with']
