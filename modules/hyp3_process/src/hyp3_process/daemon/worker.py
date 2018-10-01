7f23dedf44bc60c63c96c515a51bc3e587f3dea8
William Horn <wbhorn@alaska.edu>
2018-09-30 17:06:23 -0800

    Upload log to s3 bucket

--- modules/hyp3_process/src/hyp3_process/daemon/worker.py
+++ modules/hyp3_process/src/hyp3_process/daemon/worker.py
@@ -4,9 +4,13 @@ import sys
 import pathlib as pl
 from typing import Dict
 
+import boto3
 import asf_granule_util as gu
 
 from hyp3_events import StartEvent
+
+
+s3_client = boto3.client('s3')
 
 
 class HyP3Worker:
@@ -19,7 +23,7 @@ class HyP3Worker:
         granule_id = gu.SentinelGranule(event.granule).unique_id
         date = str(datetime.now()).replace(" ", "")
 
-        with logging(f'{granule_id}--{date}.log'):
+        with logging(name=f'{granule_id}--{date}.log', bucket=self.bucket):
             return self.processing_func(
                 event,
                 self.creds,
@@ -28,14 +32,20 @@ class HyP3Worker:
 
 
 @contextlib.contextmanager
-def logging(log_name):
+def logging(name, bucket):
     old_stdout = sys.stdout
     log = pl.Path.home() / 'log'
 
     if not log.exists():
         log.mkdir(parents=True)
 
-    with (log / log_name).open('w') as log_file:
-        sys.stdout = log_file
+    log_file = log / name
+
+    with log_file.open('w') as log:
+        sys.stdout = log
         yield
         sys.stdout = old_stdout
+
+    bucket_path = str(pl.Path('log') / name)
+
+    s3_client.upload_file(str(log_file), bucket, bucket_path)
